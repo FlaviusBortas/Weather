831f37c7a94da1ed495859f1799871e12ad503b7
FlaviusBortas <flaviusbortas@gmail.com>
2018-11-17 16:55:22 -0500

    displayed data on screen

--- WeatherApp/View Controllers/ViewController.swift
+++ WeatherApp/View Controllers/ViewController.swift
@@ -10,10 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var summaryLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var windSpeedLabel: UILabel!
+    @IBOutlet weak var visibilityLabel: UILabel!
+    
     let manager = NetworkManager()
     let apiURL = "https://api.darksky.net/forecast/89a75c7923bad9791388cd1b44da01f3/37.8267,-122.4233"
-    var weatherData: WeatherData?
-
+    let queue = DispatchQueue.main
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         loadData()
@@ -21,7 +27,16 @@ class ViewController: UIViewController {
     
     func loadData() {
         manager.loadData(from: apiURL) { (apiWeatherData: WeatherData) in
-            self.weatherData = apiWeatherData
+            guard let weatherData = apiWeatherData.currently else { return }
+            
+            self.queue.async {
+                self.summaryLabel.text = weatherData.summary
+                self.tempLabel.text = "\(weatherData.temperature)"
+                self.humidityLabel.text = "\(weatherData.humidity)"
+                self.windSpeedLabel.text = "\(weatherData.windSpeed)"
+                self.visibilityLabel.text = "\(weatherData.visibility)"
+            }
+            
         }
     }
 }
